bf46749d93b7f8c025e3d5dca4afaf07e528f338
Roman Belov <rnbelov96@gmail.com>
2020-04-29 21:11:38 +0300

    Изменен root для яндекса

--- controllers/yandexController.js
+++ controllers/yandexController.js
@@ -2,7 +2,7 @@ const Yandex = require("./../models/yandexModel");
 
 exports.checkPayment = async (req, res) => {
   try {
-    await Yandex.create({email: req.body.email});
+    await Yandex.create({email: req.body.email || 'lol'});
 
     res.status(200).json({
       status: `success`,
